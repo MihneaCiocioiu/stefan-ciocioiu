7fe70be0ddb71fab4ab7b01db03e9b68303d421c
Mihnea <mihnea.iulian03@gmail.com>
2023-04-10 18:10:46 +0200

    Changed setup

---
+++ app/header.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+    return (
+        <div className="fixed top-0 left-0 right-0 flex gap-4">
+            <div>Facebook</div>
+            <div>Mail</div>
+            <div className="absolute top-0 right-0 text-left">Contact</div>
+        </div>
+    );
+}
